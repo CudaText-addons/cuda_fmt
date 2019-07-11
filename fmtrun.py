7222a157bc3bb8a42b0b85529f133c3aa608f208
Alexey-t <support@uvviewsoft.com>
2019-07-11 11:24:12 +0300

    support n selections

--- fmtrun.py
+++ fmtrun.py
@@ -1,37 +1,51 @@
 from cudatext import *
 
+def is_selected(carets):
+
+    for c in carets:
+        if c[2]>=0:
+            return True
+    return False
+
+
 def run_format(do_format, msg, force_all):
 
     if ed.get_sel_mode() != SEL_NORMAL:
-        msg_status(msg + "Column/line selections not supported")
+        msg_status(msg + "Column selection is not supported")
         return
 
-    if force_all:
-        text = ''
-    else:
-        text = ed.get_text_sel()
+    carets = ed.get_carets()
+    use_all = force_all or not is_selected(carets)
 
-    if text:
-        with_eol = text.endswith('\n')
-        if with_eol:
-            text = text.rstrip('\n')
+    if not use_all:
+        nsel = 0
+        for x0, y0, x1, y1 in reversed(carets):
+            if y1<0: continue
+            if (y0, x0)>(y1, x1):
+                x0, y0, x1, y1 = x1, y1, x0, y0
 
-        text = do_format(text)
-        if not text:
-            msg_status(msg + "Cannot format text")
-            return
+            text = ed.get_text_substr(x0, y0, x1, y1)
+            with_eol = text.endswith('\n')
+            if with_eol:
+                text = text.rstrip('\n')
 
-        if with_eol:
-            text += '\n'
+            text = do_format(text)
+            if not text:
+                continue
 
-        x0, y0, x1, y1 = ed.get_carets()[0]
-        if (y0, x0)>(y1, x1):
-            x0, y0, x1, y1 = x1, y1, x0, y0
+            if with_eol:
+                text += '\n'
 
-        ed.set_caret(x0, y0)
-        ed.replace(x0, y0, x1, y1, text)
+            ed.set_caret(x0, y0)
+            ed.replace(x0, y0, x1, y1, text)
+            nsel += 1
 
-        msg_status(msg + "Formatted selection")
+        if nsel>1:
+            msg_status(msg + "Formatted %d selections"%nsel)
+        elif nsel==1:
+            msg_status(msg + "Formatted selection")
+        else:
+            msg_status(msg + "Cannot format selection(s)")
 
     else:
         text1 = ed.get_text_all()
@@ -44,6 +58,6 @@ def run_format(do_format, msg, force_all):
             msg_status(msg + 'Text is already formatted')
             return
 
-        msg_status(msg + "Formatted entire text")
         ed.set_caret(0, 0)
         ed.set_text_all(text)
+        msg_status(msg + "Formatted entire text")
